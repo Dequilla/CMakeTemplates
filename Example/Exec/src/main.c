d8f34c1871ab35b4ddf59de0e70e5809c503ed6a
Edwin Wallin <edwin.wallin@dequilla.com>
2024-04-04 14:40:44 +0200

    Fixed submodule issue with Example.

---
+++ Example/Exec/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <lib.h>
+
+int main(int argc, char** argv)
+{
+    printf("Hello world!");
+
+    hello_world();
+
+    return 0;
+}
---
+++ Example/Lib/src/lib.c
@@ -0,0 +1,8 @@
+#include "lib.h"
+
+#include <stdio.h>
+
+void hello_world()
+{
+    printf("Hello world!");
+}
